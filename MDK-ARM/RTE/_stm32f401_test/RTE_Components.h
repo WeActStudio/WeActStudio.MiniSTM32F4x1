3d6155ba24419f208bfb85615244702c5daae608
WeActTC <WeAct_TC@163.com>
2020-01-09 11:32:38 +0800

    Squashed 'STM32F401CCU6-Black-Board/SDK/HAL/stm32f401_test/' content from commit 83b96dc
    git-subtree-dir: STM32F401CCU6-Black-Board/SDK/HAL/stm32f401_test
    git-subtree-split: 83b96dc9b67456d8b4caeabc13ddc53baca8e87b

---
+++ MDK-ARM/RTE/_stm32f401_test/RTE_Components.h
@@ -0,0 +1,33 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'stm32f401_test' 
+ * Target:  'stm32f401_test' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+/*  Keil::Device:STM32Cube Framework:STM32CubeMX:1.0.0 */
+#define RTE_DEVICE_FRAMEWORK_CUBE_MX
+/*  Keil::Device:STM32Cube HAL:Common:1.7.6 */
+#define RTE_DEVICE_HAL_COMMON
+/*  Keil::Device:STM32Cube HAL:Cortex:1.7.6 */
+#define RTE_DEVICE_HAL_CORTEX
+/*  Keil::Device:STM32Cube HAL:GPIO:1.7.6 */
+#define RTE_DEVICE_HAL_GPIO
+/*  Keil::Device:STM32Cube HAL:PWR:1.7.6 */
+#define RTE_DEVICE_HAL_PWR
+/*  Keil::Device:STM32Cube HAL:RCC:1.7.6 */
+#define RTE_DEVICE_HAL_RCC
+
+
+#endif /* RTE_COMPONENTS_H */
---
+++ README.md
@@ -0,0 +1,4 @@
+# WeAct
+## stm32f401_test
+WeAct STM32F401 Core Board Test Program
+ ![STM32F401CCU6](https://github.com/YXZhu/stm32f401_test/blob/master/stm32.jpg)
